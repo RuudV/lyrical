7540977c45ab6c381d08868e1c684f9dfe4a6142
Ruud Voost <ruudvoost@MacBook-Pro-van-Ruud.local>
2018-09-01 22:53:44 +0200

    -

--- src/app/modules/auth/interfaces/auth-token.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface AuthToken {
-  access_token: string; // Token String
-  token_type: string; // Token Type (Usually 'Bearer'
-  expires_in: string; // Expiry time in seconds
-  state?: string; // State to check possible intervention
-}
